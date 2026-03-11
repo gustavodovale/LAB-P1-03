65e507bdc8bb7815fe9e530a496f420b93df24bd
gustavo <gust.dovale@gmail.com>
2026-03-11 17:36:48 -0300

    não usar a biblioteca touch

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-import torch # 
 
-def create_causal_mask(seq_len = 5):
-    # Criar uma máscara causal (triangular superior) para seq_len 
-    mask = touch.triu(touch.ones(seq_len, seq_len), diagonal=1)
