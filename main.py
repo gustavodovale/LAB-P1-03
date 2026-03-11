31647cd0feca912679f3dd68df725d497b0b7c37
gustavo <gust.dovale@gmail.com>
2026-03-11 20:13:44 -0300

    Criando a mascara

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+## Tarefa 1: Implementando a Máscara Causal
+
+def create_causal_mask(seq_len):
+    # Criar uma máscara causal utilizando NumPy
+    mask = np.triu(np.ones((seq_len, seq_len)), k=1) 
+    return mask
+
+seq_len = 5
+mask = create_causal_mask(seq_len)
+
+print("Máscara Causal (NumPy):")
+print(mask)
